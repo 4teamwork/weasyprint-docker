924a4c653f2890292a7785bc07b5dccf94ab9e3b
Reng van Oord <reng.vanoord@bcc.no>
2022-11-05 17:23:41 +0100

    Added reverse proxy settings

--- proxy/BccCode.PdfServer.Proxy/Program.cs
+++ proxy/BccCode.PdfServer.Proxy/Program.cs
@@ -1,8 +1,32 @@
+using Yarp.ReverseProxy.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Proxy routes
+var routes = new[]
+{
+    new RouteConfig()
+    {
+        RouteId = "route1",
+        ClusterId = "cluster1",
+        Match = new RouteMatch { Path = "{**catch-all}" }
+    }
+};
+var clusters = new[]
+{
+    new ClusterConfig()
+    {
+        ClusterId = "cluster1",
+        Destinations = new Dictionary<string, DestinationConfig>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "destination1", new DestinationConfig() { Address = "http://localhost:8080" } }
+        }
+    }
+};
 
+// Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddReverseProxy().LoadFromMemory(routes, clusters);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,5 +46,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapReverseProxy(proxy =>
+{
+    
+});
 
 app.Run();
+
+
