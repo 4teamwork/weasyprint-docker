d6e30881afd96ec9fd80ea8347d3c190de1f29fb
Piotr <czpiotr@gmail.com>
2023-01-11 21:59:25 +0100

    Show Swagger in Production

--- proxy/BccCode.PdfServer.Proxy/Program.cs
+++ proxy/BccCode.PdfServer.Proxy/Program.cs
@@ -54,12 +54,8 @@ builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICAT
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
